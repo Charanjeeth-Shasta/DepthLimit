07467ca650edc2d37e45639e44b32f15d8c8f9f5
Charanjeeth-Shasta <charanjeethshasta98@gmail.com>
2026-05-28 17:04:53 +0530

    fix: JWT cookie issue

--- depth-limit/server/src/controllers/authController.js
+++ depth-limit/server/src/controllers/authController.js
@@ -82,7 +82,7 @@ const loginUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 15 * 60 * 1000 // 15 minutes
     });
 
